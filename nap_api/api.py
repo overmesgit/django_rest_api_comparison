e0ab3ff9b1b1bff9866e713c5ec465d14e6fb70c
overmes <over>
2015-06-22 22:44:48 +0300

    django nap

--- nap_api/api.py
+++ nap_api/api.py
@@ -1,8 +1,11 @@
+import json
+
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 from nap import serialiser, rest
+from nap.rest.views import ObjectDeleteMixin
+
 from resources.models import Entry
-from django.core.exceptions import ValidationError
 
 
 class EntryForm(ModelForm):
@@ -13,11 +16,11 @@ class EntryForm(ModelForm):
 
 class EntrySerialiser(serialiser.ModelSerialiser):
     def object_inflate(self, data, instance=None, **kwargs):
-        data = super(EntrySerialiser, self).object_inflate(data, instance, **kwargs)
-        form = EntryForm(instance=data)
+        form = EntryForm(data)
         if not form.is_valid():
-            raise ValidationError(form.errors)
-        return data
+            raise ValueError(form.errors)
+        obj = super(EntrySerialiser, self).object_inflate(data, instance, **kwargs)
+        return obj
 
     def deflate_user(self, obj, data, **kwargs):
         return obj.user_id
@@ -29,9 +32,19 @@ class EntrySerialiser(serialiser.ModelSerialiser):
         model = Entry
 
 
-class EntryPublisher(rest.ModelPublisher):
+class EntryPublisher(ObjectDeleteMixin, rest.ModelPublisher):
     api_name = 'entry'
     serialiser = EntrySerialiser()
+
+    def object_delete_default(self, request, object_id, **kwargs):
+        self.serialiser._meta.model.objects.get(pk=object_id).delete()
+        return self.create_response('')
+
+    def object_put_default(self, request, object_id, **kwargs):
+        data = json.loads(request.body)
+        obj = self.serialiser.object_inflate(data, Entry.objects.get(pk=object_id))
+        obj.save()
+        return self.render_single_object(obj)
 
 
 rest.api.register('api', EntryPublisher)
