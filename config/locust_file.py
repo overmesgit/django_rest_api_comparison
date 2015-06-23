7d9cf3efd13d7adad67b922c5ccc74e8dd2cf1ab
overmes <over>
2015-06-23 21:03:15 +0300

    tastypie add always_return_data add postgres db to prod settings locust set content type for post request

--- config/locust_file.py
+++ config/locust_file.py
@@ -5,23 +5,23 @@ class RestApiGetBehavior(TaskSet):
 
     @task()
     def get_rest_framework(self):
-        self.client.get('http://localhost/rest_framework/entry/')
+        self.client.get("http://localhost/rest_framework/entry/")
 
     @task()
     def get_django_views(self):
-        self.client.get('http://localhost/django_view/entry/')
+        self.client.get("http://localhost/django_view/entry/")
 
     @task()
     def get_tastypie(self):
-        self.client.get('http://localhost/tastypie/entry/')
+        self.client.get("http://localhost/tastypie/entry/")
 
     @task()
     def get_restless(self):
-        self.client.get('http://localhost/restless/entry/')
+        self.client.get("http://localhost/restless/entry/")
 
     @task()
     def get_nap(self):
-        self.client.get('http://localhost/nap/api/entry/')
+        self.client.get("http://localhost/nap/api/entry/")
 
 
 class GetApi(HttpLocust):
@@ -29,31 +29,37 @@ class GetApi(HttpLocust):
 
 
 class RestApiPostBehavior(TaskSet):
+    headers = {'content-type': 'application/json'}
 
     @task()
     def post_rest_framework(self):
-        self.client.post('http://localhost/rest_framework/entry/',
-                         {'title': 'hello', 'content': 'content', 'user': 'http://127.0.0.1/rest_framework/user/1/'})
+        self.client.post("http://localhost/rest_framework/entry/",
+                         json={"title": "hello", "content": "content", "user": "http://127.0.0.1/rest_framework/user/1/"},
+                         headers=self.headers)
 
     @task()
-    def get_django_views(self):
-        self.client.post('http://localhost/django_view/entry/',
-                         {'title': 'hello', 'content': 'content', 'user': 'http://127.0.0.1/rest_framework/user/1/'})
+    def post_django_views(self):
+        self.client.post("http://localhost/django_view/entry/",
+                         json={"title": "hello", "content": "content", "user": 1},
+                         headers=self.headers)
 
     @task()
-    def get_tastypie(self):
-        self.client.post('http://localhost/tastypie/entry/',
-                         {'title': 'hello', 'content': 'content', 'user': 'http://127.0.0.1/rest_framework/user/1/'})
+    def post_tastypie(self):
+        self.client.post("http://localhost/tastypie/entry/",
+                         json={"title": "hello", "content": "content", "user": "/tastypie/user/1/"},
+                         headers=self.headers)
 
     @task()
-    def get_restless(self):
-        self.client.post('http://localhost/restless/entry/',
-                         {'title': 'hello', 'content': 'content', 'user': 'http://127.0.0.1/rest_framework/user/1/'})
+    def post_restless(self):
+        self.client.post("http://localhost/restless/entry/",
+                         json={"title": "hello", "content": "content", "user": 1},
+                         headers=self.headers)
 
     @task()
-    def get_nap(self):
-        self.client.post('http://localhost/nap/api/entry/',
-                         {'title': 'hello', 'content': 'content', 'user': 'http://127.0.0.1/rest_framework/user/1/'})
+    def post_nap(self):
+        self.client.post("http://localhost/nap/api/entry/",
+                         json={"title": "hello", "content": "content", "user": 1},
+                         headers=self.headers)
 
 
 class PostApi(HttpLocust):
--- config/production.py
+++ config/production.py
@@ -2,3 +2,12 @@ from settings import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rest_test',
+        'USER': 'test',
+        'PASSWORD': 'test'
+    }
+}
